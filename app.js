61bb977e28236bbe673f8b5cc18ece1a219c4417
jvbgomes <jvbg16@gmail.com>
2026-07-31 02:03:36 -0300

    feat: adiciona rotas de Appointment

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const patientRoutes = require('./routes/patientRoutes');
+const appointmentRoutes = require('./routes/appointmentRoutes');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/patients', patientRoutes);
+app.use('/appointments', appointmentRoutes);
 
 module.exports = app;
