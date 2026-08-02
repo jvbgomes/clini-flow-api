a92766df26bc57ce959f3e9346c9449a7a2dbf39
jvbgomes <jvbg16@gmail.com>
2026-08-02 16:14:21 -0300

    feat: protect patients and appointments routes with auth middleware

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const patientRoutes = require('./routes/patientRoutes');
 const appointmentRoutes = require('./routes/appointmentRoutes');
+const authRoutes = require('./routes/authRoutes');
+const authMiddleware = require('./middlewares/auth');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/patients', patientRoutes);
-app.use('/appointments', appointmentRoutes);
+app.use('/auth', authRoutes);
+app.use('/patients', authMiddleware, patientRoutes);
+app.use('/appointments', authMiddleware, appointmentRoutes);
 
 module.exports = app;
