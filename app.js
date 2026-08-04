2ab26621eaf82f34704cbbdc3ee492861bf66b92
jvbgomes <jvbg16@gmail.com>
2026-08-04 19:43:11 -0300

    fix: enable CORS for frontend requests

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 const patientRoutes = require('./routes/patientRoutes');
@@ -8,6 +9,7 @@ const authMiddleware = require('./middlewares/auth');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
